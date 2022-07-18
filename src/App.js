bf8e3b03d3350fc2fa73cdf71ee45c1f2b3284f7
franalfita <alfitaygl@gmail.com>
2022-07-18 12:09:00 -0400

    Prueba 4 CloudSales FranciscoContreras

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import Sales from './components/Sales';
+import ProductsAdmin from './components/products/ProductsAdmin';
+import ProductsAdd from './components/products/ProductsAdd';
+import ProductsEdit from './components/products/ProductsEdit';
+
+import UsersAdmin from './components/users/UsersAdmin';
+import UsersAdd from './components/users/UsersAdd';
+import UsersEdit from './components/users/UsersEdit';
+
+import ClientsAdmin from './components/clients/ClientsAdmin';
+import ClientsAdd from './components/clients/ClientsAdd';
+import ClientsEdit from './components/clients/ClientsEdit';
+
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/sales" element={<Sales />}></Route>
+        <Route path="/products" element={<ProductsAdmin />}></Route>
+        <Route path="/products/add" element={<ProductsAdd />}></Route>
+        <Route path="/products/edit" element={<ProductsEdit />}></Route>
+        <Route path="/clients" element={<ClientsAdmin />}></Route>
+        <Route path="/clients/add" element={<ClientsAdd />}></Route>
+        <Route path="/clients/edit" element={<ClientsEdit />}></Route>
+        <Route path="/users" element={<UsersAdmin />}></Route>
+        <Route path="/users/add" element={<UsersAdd />}></Route>
+        <Route path="/users/edit" element={<UsersEdit />}></Route>
+
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot  } from 'react-dom/client';
+import App from './App';
+import React, { Component }  from 'react';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
